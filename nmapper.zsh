ca42c21541635c06a22eb8a919677887c8074383
Mithlonde <88001670+Mithlonde@users.noreply.github.com>
2024-03-05 15:08:05 +0100

    Add files via upload

---
+++ nmapper.zsh
@@ -0,0 +1,36 @@
+#!/usr/bin/env zsh
+#
+# Version:              1.1
+# Nmapper Author:     	Mithlonde
+# Creation Date:        27/03/2023
+# Website:              https://github.com/Mithlonde/Nmapper
+#
+# Before running this script, set the $IP environment variable to the IP address of the target and create a folder named 'scans'.
+
+function nmapper {
+	echo "==============================================================="
+	echo "Nmapper - Initial Recon"
+	echo "by Mithlonde"
+	echo "==============================================================="
+	echo "Scanning for all open ports"
+	echo "[+] Method: nmap -p- --min-rate 10000 $IP -oN scans/all-ports.nmap"
+	nmap -p- --min-rate 10000 $IP -oN scans/all-ports.nmap && ports=$(cat scans/all-ports.nmap | grep "open" | grep -v "filtered" | cut -d "/" -f 1 | sort -u | xargs | tr ' ' ',')
+	echo "Setting environment variable for all open ports found"
+	echo "==============================================================="
+	echo "Scanning services on all open ports"
+	echo "[+] Method: nmap -p$ports -sCV $IP -oN scans/services.nmap"
+	nmap -p$ports -sCV $IP -oN scans/services.nmap
+	echo "==============================================================="
+	echo "Scanning UDP SNMP on port 161,162"
+	echo "[+] Method: nmap -p161,162 -sUCV -Pn $IP -oN scans/snmp.nmap"
+	nmap -p161,162 -sUCV -Pn $IP -oN scans/snmp.nmap
+	echo "==============================================================="
+	echo "OPTIONAL: Run a full scan in the background if required:"
+	echo "[+] Method: nmap -sCV $IP -oN scans/full-scan.nmap"
+	echo "3..2..1.."
+	echo "Intel retrieved"	
+	echo "Good luck hunting!"
+	echo "==============================================================="
+}
+	
+nmapper
