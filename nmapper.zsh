96a2023d84f29bcdc0366d8bff212959d67cc44f
Mithlonde <88001670+Mithlonde@users.noreply.github.com>
2024-03-06 18:33:36 +0100

    Uploaded Nmapper V2.1
    Issues solved:
    1. [x] Edit nmapper so when -Pn is required, it will automatically run corresponding commands.
    
    Note to trigger in nmap scan:
    "Note: Host seems down. If it is really up, but blocking our ping probes, try -Pn"
    
    Implemented ping_check:
    - Revised the handling of the -Pn option to conditionally append it based on the host's response to ping probes, ensuring uninterrupted scans. The += operator is now used to append the string -Pn to the existing value of the nmap command variables.
    	
    2. [x] Solve port order issue. Currently, service scan writes port in incorrect order: "Method: nmap -p135,139,3389,445,8080 -sCV 192.168.219.43 -Pn -oN scans/services.nmap"
    	
    Updated port sorting method in the nmapper script:
    - Changed the sorting method from `sort -u` to `sort -nu` when extracting and sorting port numbers. This change ensures that port numbers are sorted numerically in ascending order, resolving any issues with the order of numbers in the output.
    - Enhanced the ports variable extraction to include the count of open ports found, providing better context for subsequent service scans.
    
    Additional changes to the Script:
    
    - Updated the header with an ASCII art logo and a more descriptive title.
    - Implemented color-coded output for better visualization and clarity.
    - Modified the way commands are displayed to enhance readability.

--- nmapper.zsh
+++ nmapper.zsh
@@ -1,36 +1,91 @@
 #!/usr/bin/env zsh
 #
-# Version:              1.1
+# Version:              2.1
 # Nmapper Author:     	Mithlonde
-# Creation Date:        27/03/2023
+# Creation Date:        06/03/2024
 # Website:              https://github.com/Mithlonde/Nmapper
 #
 # Before running this script, set the $IP environment variable to the IP address of the target and create a folder named 'scans'.
 
+# Define color and formatting variables
+C=$(printf '\033')
+WHITE="${C}[1;37m"
+YELLOW="${C}[1;33m"
+RED="${C}[1;31m"
+BLUE="${C}[1;96m"
+NC="${C}[0m" # Neutral Color
+
+CWD=$(echo "$PWD" | sed "s|^$HOME|~|")
+echo_command="${RED}$(whoami)@$(hostname)${NC}:${WHITE}${CWD}${NC}\$"
+
 function nmapper {
-	echo "==============================================================="
-	echo "Nmapper - Initial Recon"
-	echo "by Mithlonde"
-	echo "==============================================================="
-	echo "Scanning for all open ports"
-	echo "[+] Method: nmap -p- --min-rate 10000 $IP -oN scans/all-ports.nmap"
-	nmap -p- --min-rate 10000 $IP -oN scans/all-ports.nmap && ports=$(cat scans/all-ports.nmap | grep "open" | grep -v "filtered" | cut -d "/" -f 1 | sort -u | xargs | tr ' ' ',')
-	echo "Setting environment variable for all open ports found"
-	echo "==============================================================="
-	echo "Scanning services on all open ports"
-	echo "[+] Method: nmap -p$ports -sCV $IP -oN scans/services.nmap"
-	nmap -p$ports -sCV $IP -oN scans/services.nmap
-	echo "==============================================================="
-	echo "OPTIONAL: Scanning UDP SNMP on port 161,162"
-	echo "[+] Method: sudo nmap -p161,162 -sUCV $IP -oN scans/snmp.nmap"
-	echo "==============================================================="
-	echo "OPTIONAL: Run a full scan in the background if required:"
-	echo "[+] Method: nmap -sCV $IP -oN scans/full-scan.nmap"
-	echo "==============================================================="
-	echo "3..2..1.."
-	echo "Intel retrieved"
-	echo ""	
-	echo "Happy hunting!"
+	echo ""
+	echo "    ▄   █▀▄▀█ ██   █ ▄▄  █ ▄▄  ▄███▄   █▄▄▄▄ "
+	echo "     █  █ █ █ █ █  █   █ █   █ █▀   ▀  █  ▄▀ "
+	echo " ██   █ █ ▄ █ █▄▄█ █▀▀▀  █▀▀▀  ██▄▄    █▀▀▌  "
+	echo " █ █  █ █   █ █  █ █     █     █▄   ▄▀ █  █  "
+	echo " █  █ █    █     █  █     █    ▀███▀     █   "
+	echo " █   ██   ▀     █    ▀     ▀            ▀    "
+	echo "               ▀                             "
+	echo "       Network Reconnaissance Automator      "
+	echo "                by Mithlonde                 "
+	echo ""
+
+    # Define the Nmap command as a string variable
+    port_scan="nmap -p- --min-rate 10000 $IP -oN scans/all-ports.nmap"
+	udp_scan="sudo nmap -p161,162 -sUCV $IP -oN scans/snmp.nmap"
+	full_scan="nmap -sCV $IP -oN scans/full-scan.nmap"
+
+    # Execute a ping scan to check host availability
+    ping_check=$(nmap -sn --min-rate 10000 $IP)
+
+    # Check if the ping scan output contains the indication that the host seems down
+    if echo "$ping_check" | grep -q "Note: Host seems down"; then
+        # Add the -Pn option to the Nmap command if the host seems down
+		echo "${YELLOW}[+] ${WHITE}Checking if host is up...${NC}"
+		echo -e "$echo_command ${BLUE}nmap -sn --min-rate 10000 $IP${NC}" 
+		echo "Note: Host seems down. If it is really up, but blocking our ping probes, try -Pn"
+		echo "${YELLOW}Adding -Pn option to nmap commands...${NC}"
+		echo ""
+        port_scan+=" -Pn"
+    fi
+
+    # Running addiotional scans
+	echo "${YELLOW}[+] ${WHITE}Scanning for all open ports...${NC}"
+    echo "$echo_command ${BLUE}$port_scan${NC}"
+	eval "$port_scan"
+	echo ""
+	echo "Output saved to scans/all-ports.nmap"
+
+	# Extract ports and assign them to the $ports variable
+    ports=$(cat scans/all-ports.nmap | grep "open" | grep -v "filtered" | cut -d "/" -f 1 | sort -nu | xargs | tr ' ' ',')
+	num_ports=$(echo "$ports" | tr ',' '\n' | wc -l)
+	services_scan="nmap -p$ports -sCV $IP -oN scans/services.nmap"
+	echo "${YELLOW}Done. Found $num_ports open port(s)${NC}"
+	echo ""
+
+    # If host is down, add -Pn option to services_scan command
+    if echo "$ping_check" | grep -q "Note: Host seems down"; then
+        services_scan+=" -Pn"
+		udp_scan+=" -Pn"
+		full_scan+=" -Pn"
+    fi
+
+	echo "${YELLOW}[+] ${WHITE}Scanning services on all open ports...${NC}"
+	echo "$echo_command ${BLUE}$services_scan${NC}"
+	eval "$services_scan"
+	echo ""
+	echo "Output saved to scans/all-services.nmap"
+
+	echo ""
+	echo "${YELLOW}[!] OPTIONAL: ${NC}Scanning UDP SNMP on port 161,162${NC}"
+	echo "${NC}Command: ${BLUE}$udp_scan${NC}"
+
+	echo ""
+	echo "${YELLOW}[!] OPTIONAL: ${NC}Run a another full scan in the background if preferred${NC}"
+	echo "${NC}Command: ${BLUE}$full_scan${NC}"
+	echo ""
+    echo -e "${YELLOW}Happy hunting!${NC}"
 }
-	
+
 nmapper
